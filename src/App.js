8d508221b593cd3877d706d8bea49cd7b68eca7b
Aiveteran <42783946+Aiveteran@users.noreply.github.com>
2018-10-15 13:23:58 -0700

    added code back

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      TodoList: [
+        {
+          Task: 'sleep',
+          id: 1234,
+          completed: false,
+        },
+      ],
+    };
+  }
   // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  NewTodo = add => {
+    add.preventDefault();
+    const TodoList = this.state.Todo.slice();
+    Task.push({ Task: this.state.TodoList, id: Date.now(), completed: false, });
+    this.setState({ TodoList, Task: '' });
+  };
+  changeTodo = add => {
+    this.setState({ [add.target.name]: add.target.value });
+    toggleTaskCompleted = id =>
+  }
+  //this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
@@ -14,3 +36,8 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+
+
